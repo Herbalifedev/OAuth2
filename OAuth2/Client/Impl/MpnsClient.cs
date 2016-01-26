4099cc3d80637470b0f6344f3188a989e1868658
David Lee Kuan Seong <davidlks@gmail.com>
2016-01-26 12:52:35 +0800

    Added client implementation for MPNS (MTS Push Notification Service)

---
+++ OAuth2/Client/Impl/MpnsClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using OAuth2.Configuration;
+using OAuth2.Infrastructure;
+using OAuth2.Models;
+
+namespace OAuth2.Client.Impl
+{
+    public class MpnsClient : OAuth2Client
+    {
+        public MpnsClient(IRequestFactory factory, IClientConfiguration configuration) : base(factory, configuration)
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "MPNS";
+            }
+        }
+
+        protected override Endpoint AccessCodeServiceEndpoint
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override Endpoint AccessTokenServiceEndpoint
+        {
+            get
+            {
+                return new Endpoint
+                {
+                    BaseUri = "http://mykl-dts-mac04:3000",
+                    Resource = "/wlc/token"
+                };
+            }
+        }
+
+        protected override Endpoint UserInfoServiceEndpoint
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override UserInfo ParseUserInfo(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
